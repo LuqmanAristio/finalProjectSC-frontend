961bd315d8377662f9e9d6e60137874cbe1bbac9
LuqmanAristio <aristio170802@gmail.com>
2023-12-18 13:29:44 +0800

    update UI Nav

--- app/component/Main.tsx
+++ app/component/Main.tsx
@@ -24,7 +24,6 @@ export default function Main() {
             setSelectedFile(file);
             setLokal(false);
         } else {
-            // Handle the case when files are null or empty
             console.error("No files selected");
         }
     };
@@ -63,7 +62,7 @@ export default function Main() {
                     className='w-3/5 mx-auto mb-5'
                 />
                 <div className='flex flex-col w-4/5 mx-auto'>
-                    <h1 className='text-5xl font-extrabold text-[#282828]'>Lung Cancer Diagnosis <br /> Based on CT Scan Image</h1>
+                    <h1 className='text-5xl font-extrabold text-[#282828] leading-tight'>Lung Cancer Diagnosis <br /> Based on CT Scan Image</h1>
                     <h3 className='text-2xl mt-10 font-semibold text-[#4D4D4D]'>100% Automatically and <span className='text-white bg-[#DB3A58] px-5 py-2 rounded-md'>Free</span></h3>
                 </div>
             </div>
@@ -93,7 +92,7 @@ export default function Main() {
                         onChange={handleFileChange}
                     />
                     
-                    <h4 className='absolute bottom-32 text-lg text-[#727272]'>or drag and drop a file</h4>
+                    <h4 className='absolute bottom-32 text-lg text-[#727272]'>or drag and drop a file here</h4>
                 </div>
                 <div className='w-5/6 mt-5'>
                     <h4 className='text-[#4D4D4D]'><span className='font-bold'>Note</span> : The system built is not always accurate, it is only used for initial diagnosis.  The final decision is in the hands of the expert</h4>
--- app/component/Navigation.tsx
+++ app/component/Navigation.tsx
@@ -1,19 +1,11 @@
 export default function Navigation() {
     return(
-        <div className='w-full bg-white h-fit nav flex justify-between py-6 px-40 text-[#2D2D2D]'>
+        <div className='w-full bg-white h-fit nav flex items-center justify-between py-6 px-40 text-[#2D2D2D]'>
             <h1 className='text-2xl font-extrabold'>Lung Cancer Diagnosis AI System</h1>
-            <div className='flex w-2/6 items-center justify-between'>
-                <a href="#">
-                    <h3 className='font-semibold text-xl'>Home</h3>
-                </a>
-
-                <a href="#">
-                    <h3 className='font-semibold text-xl'>About</h3>
-                </a>
+            <div className='flex w-2/6 items-center justify-end'>
+            
+                <h3 className='font-semibold text-xl bg-[#DB3A58] text-white px-12 rounded py-2'>Soft Computing Final Project</h3>
                 
-                <a href="#">
-                    <h3 className='font-semibold text-xl'>Developer</h3>
-                </a>
             </div>
         </div>
     )
